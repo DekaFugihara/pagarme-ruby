bef3419397aaec2b04e6119b0a810d26e2ee7171
Denise Fugihara <contato@denika.com.br>
2016-05-14 00:02:11 -0300

    Revert "remove api_key from this gem"
    This reverts commit afc1c82ca3f73bf45164f529469f3f731fb6b48a.

--- lib/pagarme.rb
+++ lib/pagarme.rb
@@ -24,11 +24,7 @@ module PagarMe
   self.api_endpoint = 'https://api.pagar.me/1'
   self.open_timeout = 30
   self.timeout      = 90
-  if Rails.env.production?
-    self.api_key    = ENV['PAGARME_API_KEY']
-  else
-    self.api_key    = ENV['PAGARME_API_KEY']
-  end
+  self.api_key      = "ak_test_Ytru3s0YBc6Z5mDkGrBr6nIn1YLaGh"
 
   # TODO: Remove deprecated PagarMe.validate_fingerprint
   def self.validate_fingerprint(*args)
